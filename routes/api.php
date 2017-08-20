6f38b06f88e5a4e0473da053e1bb4bd98d244735
alincode <alincode@gmail.com>
2017-08-20 11:33:24 +0800

    update controller

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('api')->get('/article', 'ArticleController@index');
+Route::middleware('api')->get('/user', 'UserController@index');
